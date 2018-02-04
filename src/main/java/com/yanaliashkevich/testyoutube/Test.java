a31c5309f0312dc4f79600f40d9ea9979250238c
YanaLiashkevich <yana-devchonka@mail.ru>
2018-02-04 12:28:15 +0300

    first try to commit sout

---
+++ src/main/java/com/yanaliashkevich/testyoutube/Test.java
@@ -0,0 +1,10 @@
+package com.yanaliashkevich.testyoutube;
+
+/**
+ * Created by Yana on 04.02.2018.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
