5c1e3d281f25e4860b6b888568597c0a015e75ff
YanaLiashkevich <yana-devchonka@mail.ru>
2020-01-04 11:52:51 +0300

    Revert "added sout"

--- src/main/java/com/yanaliashkevich/searchchannels/Main.java
+++ src/main/java/com/yanaliashkevich/searchchannels/Main.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start the program...");
         ArrayList<Channel> listOfChannels = Searcher.searchChannels();
         Searcher.printChannels(listOfChannels);
     }
