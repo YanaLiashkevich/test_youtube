9edb31215aaf98fb7a939596f5d24939abf7f9db
YanaLiashkevich <yana-devchonka@mail.ru>
2020-01-04 11:39:59 +0300

    added sout

--- src/main/java/com/yanaliashkevich/searchchannels/Main.java
+++ src/main/java/com/yanaliashkevich/searchchannels/Main.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Start the program...");
         ArrayList<Channel> listOfChannels = Searcher.searchChannels();
         Searcher.printChannels(listOfChannels);
     }
